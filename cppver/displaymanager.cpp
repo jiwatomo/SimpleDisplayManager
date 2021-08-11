2f8b1f4581df9f832fb70f44ef3228bb10d937ff
jiwatomo <jiwatomo@protonmail.com>
2021-08-11 12:48:49 +0700

    resolution changing complite

--- cppver/displaymanager.cpp
+++ cppver/displaymanager.cpp
@@ -2,14 +2,32 @@
 #include <conio.h>
 #include <string>
 using namespace std;
+int changeto1080p();
+int changeto768();
 int main() {
 	while (1) {
-		string menu = "1.change resolution mode \n2.change projectmode \n";
+		string menu = "1.Change resolution mode \n2.Change projectmode \n";
 		cout<<menu<<endl;
 		char inputer = _getch();
 		if(inputer == '1') {
-			string menu1 = "";
-			
+			string menu1 = "1.Change to 1080p\n2.Change to 1366 mode";
+			cout<<menu1<<endl;
+			char inputer1 = _getch();
+			if (inputer1 == '1') {
+				int check = changeto1080p();
+				if (check == 1) {
+					cout<<"success"<<endl;
+				} else {
+					cout<<"failed changing to 1080p"<<endl;
+				}
+			} else if (inputer1 == '2') {
+				int check = changeto768();
+				if (check == 1) {
+					cout<<"success"<<endl;
+				} else {
+					cout<<"failde changing to 1366 mode"<<endl;
+				}
+			}
 		} else if (inputer == '2') {
 			string menu2= "";
 		} else {
