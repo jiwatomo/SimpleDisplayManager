7b0a0f8591cc0b913393314e933694e6197cb60f
jiwatomo <jiwatomo@protonmail.com>
2021-08-11 12:32:11 +0700

    cpp declarate error

---
+++ cppver/displaymanager.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+using namespace std;
+int main() {
+	while (1) {
+		String menu = "1.change resolution mode \n2.change projectmode \n";
+		cout<<menu<<endl;
+		inputer = _getch();
+		if(inputer == "1") {
+			String menu1 = "";
+			
+		} else if (inputer == "2") {
+			String menu2= "";
+		} else {
+			break;
+		}
+		cout<<"Bye"<<endl;
+	}
+	return 0;
+}
+
+int changeto1080p() {
+	return 1;
+}
+int changeto768() {
+	return 1;
+}
+int changetoExtend() {
+	return 1;
+}
+int changetoOnly1() {
+	return 1;
+}
+int changetoOnly2() {
+	return 1;
+}
